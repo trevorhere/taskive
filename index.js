62bc946d1e9350c567ff8fa15fa9dccf0a7bc25b
trevorhere <trevoranthonylane@gmail.com>
2017-10-27 19:05:16 -0600

    stuff

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var ngrok = require('ngrok');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-ngrok.connect(function (err, url) {});
-//var request = require("request");
-//var http = require('http');
 var twilio = require('twilio');
-//var bodyParser = require("body-parser");
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -22,11 +16,14 @@ var client = require('twilio')(accountSid, authToken);
 app.set('port', process.env.PORT || 8080);
 
 
-
+//TEST FUNCTIONS
 function sayHello(){
   console.log("hello");
 }
 
+
+
+//TEST FUNCTIONS
 function sendText(){
   client.messages.create({
       body: 'Greetings! The current time is: 20:03 MST IPV5EL1VGWFMLDS',
@@ -36,32 +33,17 @@ function sendText(){
   });
 }
 
-//
-// app.post('/message', function(req,res){
-//   console.log(req.body);
-//   var msgFrom = req.body.From;
-//    var msgBody = req.body.Body;
-//
-//    sayHello();
-//
-//    res.send(`
-//      <Response>
-//       </Message>
-//           Hello ${msgFrom}. You said: ${msgBody}
-//       </Message>
-//      </Response>
-//      `);
-// });
 
+//INDEX ROUTE
 app.get('/', function(req, res){
         res.send('hello world');
         sayHello();
         sendText();
         sayHello();
-
 });
 
 
+//RECIEVE MESSAGE
 app.post('/message', (req, res) => {
 
   // Start our TwiML response.
@@ -71,21 +53,6 @@ app.post('/message', (req, res) => {
    console.log(msgBody);
   // Add a text message.
   const msg = twiml.message('What is up dude?');//
-// app.post('/message', function(req,res){
-//   console.log(req.body);
-//   var msgFrom = req.body.From;
-//    var msgBody = req.body.Body;
-//
-//    sayHello();
-//
-//    res.send(`
-//      <Response>
-//       </Message>
-//           Hello ${msgFrom}. You said: ${msgBody}
-//       </Message>
-//      </Response>
-//      `);
-// });
 
   // Add a picture message.
   msg.media('https://www.softpaws.com/template/images/landing_page/july_cat_image.jpg');
@@ -94,13 +61,9 @@ app.post('/message', (req, res) => {
   res.end(twiml.toString());
 });
 
-    // client.messages.create({
-    //     body: 'Greetings! The current time is: 20:03 MST IPV5EL1VGWFMLDS',
-    //     to: '+15598167525',  // Text this number
-    //     from: '+13852904244' // From a valid Twilio number
-    //
-    // });
 
-    app.listen(app.get('port'), function(){
-      console.log("app is running on: " + app.get('port'))
-    });
+
+
+app.listen(app.get('port'), function(){
+  console.log("app is running on: " + app.get('port'))
+});
